8a8144992492df62604d4fd9ad284e7720a2e959
Soonnk <angel.g.hernandez.1997@gmail.com>
2019-04-06 11:57:18 -0600

    Proyecto desde 0

---
+++ ReservacionHotelera/Entidades/Reservaciones.vb
@@ -0,0 +1,11 @@
+﻿Public Class Reservaciones
+    Private id As Integer
+    Private numeroHabitaciones As Integer
+    Private formaPago As String
+    Private fechaLlegada As Date
+    Private horaLlegada As DateTime
+    Private fechaSalida As Date
+    Private horaSalida As DateTime
+    Private precioTotal As Decimal
+
+End Class
