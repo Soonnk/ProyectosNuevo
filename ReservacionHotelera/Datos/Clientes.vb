e9f019a8ad6407c4bb16d9e3f2e026f1a541036d
banellyrobles <roblesbanelly@gmail.com>
2019-04-08 18:32:10 -0500

    Capa Datos Clientes

--- ReservacionHotelera/Datos/Clientes.vb
+++ ReservacionHotelera/Datos/Clientes.vb
@@ -12,15 +12,58 @@
         End Try
     End Sub
 
+    Public Function CargarCliente(ByVal _id) As Entidades.Clientes
+        Dim query As String = "SELECT * FROM Clientes WHERE Id = " & _id
+        Try
+            ds = obj.CargarDatos(query)
+
+            Dim oEntidad As New Entidades.Clientes
+            With ds.Tables("Datos").Rows(0)
+                oEntidad.Nombre = .Item("nombre")
+                oEntidad.Apellido = .Item("apellido")
+                oEntidad.Direccion = .Item("direccion")
+                oEntidad.Municipio = .Item("municipio")
+                oEntidad.Estado = .Item("estado")
+                oEntidad.Pais = .Item("pais")
+                oEntidad.Telefono = .Item("Telefono")
+                oEntidad.Correo = .Item("correo")
+            End With
+            Return oEntidad
+        Finally
+
+
+        End Try
+    End Function
+
     Public Function InsertarCliente(ByVal entCliente As Entidades.Clientes) As Boolean
         Dim Query As String
         Try
             Query = "INSERT INTO Cliente(nombre,apellido,direccion,municipio,estado,pais,telefono,correo) VALUES ('" & entCliente.Nombre & "','" & entCliente.Apellido & "','" & entCliente.Direccion & "','" & entCliente.Municipio & "', '" & entCliente.Estado & "','" & entCliente.Pais & "','" & entCliente.Telefono & "', '" & entCliente.Correo & "')"
+            Return obj.commandSQL(Query)
         Catch ex As Exception
-
+            Return Nothing
         End Try
-
-        Return 0
     End Function
+
+    Public Sub Update()
+        Try
+            obj.Update()
+        Finally
+        End Try
+    End Sub
+
+
+    Public Function Borrar(ByVal _id As Integer) As Boolean
+        Dim Query As String
+        Try
+            Query = "DELETE FROM Clientes WHERE Id = " & _id
+            obj.commandSQL(Query)
+
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+
+
 
 End Class
