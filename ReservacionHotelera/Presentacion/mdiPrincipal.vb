4aeedca58497dc4269135b713f5f889f4727fd63
DanielChaPre <danchavez197@gmail.com>
2019-04-11 17:13:10 -0500

    gfdg

--- ReservacionHotelera/Presentacion/mdiPrincipal.vb
+++ ReservacionHotelera/Presentacion/mdiPrincipal.vb
@@ -1,30 +1,3 @@
 ﻿Public Class mdiPrincipal
-    'Habitacion
-    Private Sub AgregarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AgregarToolStripMenuItem.Click
-        pAgregarHabitaciones.ShowDialog()
-    End Sub
 
-    Private Sub ConsultarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ConsultarToolStripMenuItem.Click
-        pConsultarHabitaciones.ShowDialog()
-    End Sub
-
-
-    'Cliente
-    Private Sub AgregarToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles AgregarToolStripMenuItem1.Click
-        pAgregarClientes.ShowDialog()
-    End Sub
-
-    Private Sub ConsultarToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ConsultarToolStripMenuItem1.Click
-        pConsultarClientes.ShowDialog()
-    End Sub
-
-
-    'Reservacion
-    Private Sub AgregarToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles AgregarToolStripMenuItem2.Click
-        pAgregarReservaciones.ShowDialog()
-    End Sub
-
-    Private Sub ConsultarToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ConsultarToolStripMenuItem2.Click
-        pConsultarReservaciones.ShowDialog()
-    End Sub
 End Class
