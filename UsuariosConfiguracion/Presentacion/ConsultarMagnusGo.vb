6e1323b911b9656f0caea204a3c5bca0384cec32
anmar99 <juan.angel1818@gmail.com>
2019-05-02 12:32:37 -0500

    cargar magnus go

--- UsuariosConfiguracion/Presentacion/ConsultarMagnusGo.vb
+++ UsuariosConfiguracion/Presentacion/ConsultarMagnusGo.vb
@@ -1,5 +1,20 @@
 ﻿Public Class ConsultarMagnusGo
     Dim onegocio As New Negocios.Tienda
+    Private ReadOnly Property mdiPrincipal1() As mdiPrincipal
+        Get
+            If Me.ParentForm Is Nothing Then
+                Return Nothing
+            End If
+
+            Return TryCast(Me.ParentForm, mdiPrincipal)
+        End Get
+    End Property
+
+
+    Private Sub ConsultarEmpresa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.mdiPrincipal1.activo = "magnusgo"
+        Cargar()
+    End Sub
 
     Public Sub Cargar()
         Dim dt As New DataTable
