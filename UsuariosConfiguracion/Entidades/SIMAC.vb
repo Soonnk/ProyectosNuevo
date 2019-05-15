a51131f208879263925bc24fb136f54251241b88
anmar99 <juan.angel1818@gmail.com>
2019-05-15 10:45:13 -0500

    .

--- UsuariosConfiguracion/Entidades/SIMAC.vb
+++ UsuariosConfiguracion/Entidades/SIMAC.vb
@@ -1,3 +1,263 @@
 ﻿Public Class SIMAC
+    Private _OID As Integer
+    Public Property OID() As Integer
+        Get
+            Return _OID
+        End Get
+        Set(ByVal value As Integer)
+            _OID = value
+        End Set
+    End Property
+
+    Private _SistemaAdmon As String
+    Public Property SistemaAdmon() As String
+        Get
+            Return _SistemaAdmon
+        End Get
+        Set(ByVal value As String)
+            _SistemaAdmon = value
+        End Set
+    End Property
+
+    Private _NoEmpresa As String
+    Public Property NoEmpresa() As String
+        Get
+            Return _NoEmpresa
+        End Get
+        Set(ByVal value As String)
+            _NoEmpresa = value
+        End Set
+    End Property
+
+    Private _ProductoTerminado As String
+    Public Property ProductoTerminado() As String
+        Get
+            Return _ProductoTerminado
+        End Get
+        Set(ByVal value As String)
+            _ProductoTerminado = value
+        End Set
+    End Property
+
+    Private _MateriaPrima As String
+    Public Property MateriaPrima() As String
+        Get
+            Return _MateriaPrima
+        End Get
+        Set(ByVal value As String)
+            _MateriaPrima = value
+        End Set
+    End Property
+
+    Private _MateriaTalla As String
+    Public Property MateriaTalla() As String
+        Get
+            Return _MateriaTalla
+        End Get
+        Set(ByVal value As String)
+            _MateriaTalla = value
+        End Set
+    End Property
+
+    Private _Fracciones As String
+    Public Property Fracciones() As String
+        Get
+            Return _Fracciones
+        End Get
+        Set(ByVal value As String)
+            _Fracciones = value
+        End Set
+    End Property
+
+    Private _Procesos As String
+    Public Property Procesos() As String
+        Get
+            Return _Procesos
+        End Get
+        Set(ByVal value As String)
+            _Procesos = value
+        End Set
+    End Property
+
+    Private _Modelos As String
+    Public Property Modelos() As String
+        Get
+            Return _Modelos
+        End Get
+        Set(ByVal value As String)
+            _Modelos = value
+        End Set
+    End Property
+
+    Private _Tabla1 As String
+    Public Property Tabla1() As String
+        Get
+            Return _Tabla1
+        End Get
+        Set(ByVal value As String)
+            _Tabla1 = value
+        End Set
+    End Property
+
+    Private _Tabla2 As String
+    Public Property Tabla2() As String
+        Get
+            Return _Tabla2
+        End Get
+        Set(ByVal value As String)
+            _Tabla2 = value
+        End Set
+    End Property
+
+    Private _Tabla3 As String
+    Public Property Tabla3() As String
+        Get
+            Return _Tabla3
+        End Get
+        Set(ByVal value As String)
+            _Tabla3 = value
+        End Set
+    End Property
+
+    Private _Tabla4 As String
+    Public Property Tabla4() As String
+        Get
+            Return _Tabla4
+        End Get
+        Set(ByVal value As String)
+            _Tabla4 = value
+        End Set
+    End Property
+
+    Private _Tabla5 As String
+    Public Property Tabla5() As String
+        Get
+            Return _Tabla5
+        End Get
+        Set(ByVal value As String)
+            _Tabla5 = value
+        End Set
+    End Property
+
+    Private _Tabla6 As String
+    Public Property Tabla6() As String
+        Get
+            Return _Tabla6
+        End Get
+        Set(ByVal value As String)
+            _Tabla6 = value
+        End Set
+    End Property
+
+    Private _FolioPedidoA As Integer
+    Public Property FolioPedidoA() As Integer
+        Get
+            Return _FolioPedidoA
+        End Get
+        Set(ByVal value As Integer)
+            _FolioPedidoA = value
+        End Set
+    End Property
+
+    Private _FolioPedidoB As Integer
+    Public Property FolioPedidoB() As Integer
+        Get
+            Return _FolioPedidoB
+        End Get
+        Set(ByVal value As Integer)
+            _FolioPedidoB = value
+        End Set
+    End Property
+
+    Private _FolioOrdenA As Integer
+    Public Property FolioOrdenA() As Integer
+        Get
+            Return _FolioOrdenA
+        End Get
+        Set(ByVal value As Integer)
+            _FolioOrdenA = value
+        End Set
+    End Property
+
+    Private _FolioOrdenB As Integer
+    Public Property FolioOrdenB() As Integer
+        Get
+            Return _FolioOrdenB
+        End Get
+        Set(ByVal value As Integer)
+            _FolioOrdenB = value
+        End Set
+    End Property
+
+    Private _CantAvance As Boolean
+    Public Property CantAvance() As Boolean
+        Get
+            Return _CantAvance
+        End Get
+        Set(ByVal value As Boolean)
+            _CantAvance = value
+        End Set
+    End Property
+
+    Private _AutoAvance As Boolean
+    Public Property AutoAvance() As Boolean
+        Get
+            Return _AutoAvance
+        End Get
+        Set(ByVal value As Boolean)
+            _AutoAvance = value
+        End Set
+    End Property
+
+    Private _FechaAvance As Boolean
+    Public Property FechaAvance() As Boolean
+        Get
+            Return _FechaAvance
+        End Get
+        Set(ByVal value As Boolean)
+            _FechaAvance = value
+        End Set
+    End Property
+
+    Private _MultiAlmacen As Boolean
+    Public Property MultiAlmacen() As Boolean
+        Get
+            Return _MultiAlmacen
+        End Get
+        Set(ByVal value As Boolean)
+            _MultiAlmacen = value
+        End Set
+    End Property
+
+    Private _MateriaAlmacen As Integer
+    Public Property MateriaAlmacen() As Integer
+        Get
+            Return _MateriaAlmacen
+        End Get
+        Set(ByVal value As Integer)
+            _MateriaAlmacen = value
+        End Set
+    End Property
+
+    Private _ProductoAlmacen As Integer
+    Public Property ProductoAlmacen() As Integer
+        Get
+            Return _ProductoAlmacen
+        End Get
+        Set(ByVal value As Integer)
+            _ProductoAlmacen = value
+        End Set
+    End Property
+
+    Private _Reporte As Integer
+    Public Property Reporte() As Integer
+        Get
+            Return _Reporte
+        End Get
+        Set(ByVal value As Integer)
+            _Reporte = value
+        End Set
+    End Property
+
 
 End Class
